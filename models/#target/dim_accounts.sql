04cb3640c24f5cf133f188841d1fc5bd6e6d2201
unknown <kamatchip@hexaware.com>
2025-03-06 11:14:07 +0000

    target1

--- models/#target/dim_accounts.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT row_number() over (order by YEAROFACCOUNT) as id, *  FROM (select  DISTINCT 
-ACCOUNTNO,
-ACCOUNTTYPE ,
-YEAROFACCOUNT ,
-CURRENT_TIMESTAMP as CREATED_AT
-from {{ ref('src_business_units') }} )
